a20077fc8c97cb5b28a4e8ec174c55a18b4d039b
levitt <karimduwest@gmail.com>
2019-01-30 09:00:59 +0100

    SOAP

--- src/main/java/com/example/restserverreact/DatabaseLoader.java
+++ src/main/java/com/example/restserverreact/DatabaseLoader.java
@@ -1,5 +1,6 @@
 package com.example.restserverreact;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
